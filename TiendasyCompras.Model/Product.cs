703641ab19468e1b6736a0a6fc3c39009b30bdf1
Raul Carlos Abelardo Rivera Castañeda <raulriverac25@outlook.com>
2022-10-24 00:52:07 -0500

    Publicacion en la nube Azure

--- TiendasyCompras.Model/Product.cs
+++ TiendasyCompras.Model/Product.cs
@@ -5,7 +5,7 @@
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ProductTypeId { get; set; }
+        public int ProductTypeId { get; set; }
         public string Brand { get; set; }
         public string Price { get; set; }
         public int Stock { get; set; }
